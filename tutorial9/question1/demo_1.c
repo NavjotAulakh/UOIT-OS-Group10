1a27a07bdeb4199c9c30224f5ed21b75f1a41532
Abdi Ibrahim <abdirisak.ibrahim@uoit.net>
2018-03-29 20:34:29 -0400

    Question 1 complete

--- tutorial9/question1/demo_1.c
+++ tutorial9/question1/demo_1.c
@@ -3,6 +3,7 @@
 int main (int argc, char *argv[])
 {
     printf("This statement will always print.\n");
+    // Uses OpenMP library to print the statement in parallel with the above print statement when '-fopenmp' is used when compiled
     #ifdef _OPENMP
     printf("This statement will only print when compiled with \"-fopenmp\"\n");
     #endif
--- tutorial9/question1/demo_2.c
+++ tutorial9/question1/demo_2.c
@@ -4,16 +4,20 @@ int main (int argc, char *argv[])
 {
     int num = 0; // Watch out for race conditions!
     int thread_num, nthreads = 16;
+    // identifies the number of threads to create (16) when compiled using OpenMP
     #ifdef _OPENMP
     omp_set_num_threads(nthreads);
     #endif
     printf("Testing OpenMP, you should see each thread print...\n");
+    // forks additional threads
     #pragma omp parallel
     {
+        // omp critical instructs code to run section to be executed by a single thread at a time
         #pragma omp critical
         {
             thread_num = omp_get_thread_num();
 
+            // will increment num and print statement if openmp was used on compile
             #ifdef _OPENMP
             num += 1;
             printf("This thread = %d, num = %d\n", thread_num, num);
